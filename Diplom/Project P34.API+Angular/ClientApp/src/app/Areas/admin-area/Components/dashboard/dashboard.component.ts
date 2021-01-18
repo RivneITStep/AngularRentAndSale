b0991b4cadb8cbaf60a44cc4263981ce72fd7555
bohdan20020808 <bohdan20020808@gmail.com>
2021-01-19 00:36:37 +0200

    'Dashboard' with JS

--- Diplom/Project P34.API+Angular/ClientApp/src/app/Areas/admin-area/Components/dashboard/dashboard.component.ts
+++ Diplom/Project P34.API+Angular/ClientApp/src/app/Areas/admin-area/Components/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -10,14 +9,43 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  
-    
+  ngOnInit():void {
+    document.getElementById("defActionOpen").click();
+function chooseAction(evt, actionName) {
+  var i, tabcontentAction, tablinksAction;
+
+  tabcontentAction = document.getElementsByClassName("tabcontent-action");
+  for (i = 0; i < tabcontentAction.length; i++) {
+    tabcontentAction[i].style.display = "none";
   }
 
-
-  sendDataToNewUser(){
-    
+  tablinksAction = document.getElementsByClassName("tablink-action");
+  for (i = 0; i < tablinksAction.length; i++) {
+    tablinksAction[i].className = tablinksAction[i].className.replace(" active", "");
   }
-   
+
+  document.getElementById(actionName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defListOpen").click();
+function openList(evt, listName) {
+  var i, tabcontentList, tablinkList;
+
+  tabcontentList = document.getElementsByClassName("tabcontent-list");
+  for (i = 0; i < tabcontentList.length; i++) {
+    tabcontentList[i].style.display = "none";
+  }
+
+  tablinkList = document.getElementsByClassName("tablink-list");
+  for (i = 0; i < tablinkList.length; i++) {
+    tablinkList[i].className = tablinkList[i].className.replace(" active", "");
+  }
+
+  document.getElementById(listName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+  }
+
+  sendDataToNewUser() {}
 }
