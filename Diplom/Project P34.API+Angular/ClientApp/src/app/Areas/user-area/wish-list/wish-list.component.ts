621d7d72c037468becfafd9cad7930eb5b8a894f
veremeychukden <veremeychukden@gmail.com>
2021-03-19 01:27:49 +0200

    Add remove product from wishlist, varification product in wishlist, product-view from wishlist

--- Diplom/Project P34.API+Angular/ClientApp/src/app/Areas/user-area/wish-list/wish-list.component.ts
+++ Diplom/Project P34.API+Angular/ClientApp/src/app/Areas/user-area/wish-list/wish-list.component.ts
@@ -1,4 +1,8 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { NotifierService } from 'angular-notifier';
+import { ProductItem } from 'src/app/home/product/product-view/model/product-item.model';
+import { ProductService } from 'src/app/home/product/product-view/service/product.service';
 
 @Component({
   selector: 'app-wish-list',
@@ -7,21 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishListComponent implements OnInit {
 
-  constructor() {
+  constructor(private notifier: NotifierService,
+    private productService: ProductService) {
     this.showStorage = JSON.parse(localStorage.getItem("wishlist"));
+    this.isEmpty();
   }
-
-
-
-  public showStorage = "";
-
-  public tmp = "";
-
-  removeFromWishList(){
-    this.showStorage
-  }
+  showStorage: ProductItem[] = [];
 
   ngOnInit() {
+  }
+  
+
+  removeFromWishList(id: string) {
+    var toDelete = this.showStorage.findIndex(t => t.id === id);
+    this.showStorage.splice(toDelete, 1);
+    localStorage.removeItem('wishlist');
+    localStorage.setItem('wishlist', JSON.stringify(this.showStorage));
+
+  }
+
+  isEmpty() {
+    const wishlist = localStorage.getItem('wishlist');
+    if (wishlist !== "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  viewProduct(id: string){
+    this.productService.temp = id;
   }
 
 }
--- Diplom/Project P34.API+Angular/ClientApp/src/app/home/product/product.component.ts
+++ Diplom/Project P34.API+Angular/ClientApp/src/app/home/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ProductService } from './product-view/service/product.service';
 import { ProductItem } from './product-view/model/product-item.model';
 import { WishListModel } from '../../Models/wishlist.model';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-product',
@@ -11,82 +12,95 @@ import { WishListModel } from '../../Models/wishlist.model';
 export class ProductComponent implements OnInit {
 
   //viewProducts: ViewedProductModel[] = [];
- // model: ViewedProductModel = new ViewedProductModel();
-  
+  // model: ViewedProductModel = new ViewedProductModel();
+
   products: ProductItem[] = [];
   product: ProductItem = new ProductItem();
 
   listOfData: ProductItem[] = [];
-searchProd:string;
+  searchProd: string;
   searchResult: ProductItem[] = [];
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService,
+    private notifier: NotifierService) {
 
-    
 
-        // this.productService.searchProduct(this.productService.search).subscribe((data2: ProductItem[])=>{
-        //   this.products = data2;
-        //   this.listOfData = data2;
-        //   this.searchResult = data2;
 
-        // });
+    // this.productService.searchProduct(this.productService.search).subscribe((data2: ProductItem[])=>{
+    //   this.products = data2;
+    //   this.listOfData = data2;
+    //   this.searchResult = data2;
 
-  }/*Constructor close*/ 
+    // });
 
-// SearchProduct() {
-//     this.searchResult = this.listOfData.filter(
-//       t => t.name.toLowerCase().includes(this.searchProd.toLowerCase())
-//     );
-//   }
+  }/*Constructor close*/
+
+  // SearchProduct() {
+  //     this.searchResult = this.listOfData.filter(
+  //       t => t.name.toLowerCase().includes(this.searchProd.toLowerCase())
+  //     );
+  //   }
 
 
   wishlistArray: ProductItem[] = [];
-  
+
 
   ngOnInit() {
     this.productService.getProducts().subscribe(
       (data: ProductItem[]) => {
         this.products = data;
-        this.listOfData = data;   
+        this.listOfData = data;
       }
     );
 
-    this.productService.es.subscribe(text=>
-      {
-        this.productService.searchProduct(text).subscribe(data => {
-          this.products = data;
-        });
+    this.productService.es.subscribe(text => {
+      this.productService.searchProduct(text).subscribe(data => {
+        this.products = data;
       });
+    });
 
-      const tmp = localStorage.getItem("wishlist");
-      if(tmp != null){
-        this.wishlistArray = JSON.parse(localStorage.getItem("wishlist"));
-      }
+    const tmp = localStorage.getItem("wishlist");
+    if (tmp != null) {
+      this.wishlistArray = JSON.parse(localStorage.getItem("wishlist"));
+    }
 
 
-      // this.productService.searchProduct(text).subscribe((data2:ProductItem[])=>{
-      //   this.searchResult=data2;
-      // });
-        
-   
+    // this.productService.searchProduct(text).subscribe((data2:ProductItem[])=>{
+    //   this.searchResult=data2;
+    // });
+
+
   }
 
-
-  wishList: WishListModel = new WishListModel();
-  viewProduct(id: string){
+  viewProduct(id: string) {
     this.productService.temp = id;
   }
 
-  addToWishList(id: string){
-    this.productService.getProductById(id).subscribe(
-      (data: ProductItem) => {
-        this.wishlistArray.push(data);
-        localStorage.setItem("wishlist", JSON.stringify(this.wishlistArray));
+  wishList: WishListModel = new WishListModel();
+
+  showStorage = [];  
+
+  addToWishList(id: string) {
+    this.showStorage = JSON.parse(localStorage.getItem("wishlist"));
+    var tmp = this.showStorage.find(t=> id === id);
+    if(tmp === undefined){
+
+      this.productService.getProductById(id).subscribe(
+        (data: ProductItem) => {
+          this.wishlistArray.push(data);
+          localStorage.setItem("wishlist", JSON.stringify(this.wishlistArray));
+          this.notifier.notify('success', 'Продукт добавлений у бажане');
+        }
+      );
+    }
+      else{
+        this.notifier.notify('warning', 'Продукт уже знаходиться у бажаному!');
       }
-    );
+
+
   }
 
-  
 
-  
+
+
 }
